a8d9743a15e1fd270d6e7e358b2b8201a05d80d5
Abby-Mitchell <abby.mitchell@btinternet.com>
2021-08-03 15:23:23 +0100

    Attempted to fix param issue

--- test/ipynb/results.py
+++ test/ipynb/results.py
@@ -230,7 +230,7 @@ class Results:
 
 def get_results(type):
     """Collects and returns the similarity results for MPL circuit tests."""
-    result_type = f"mpl/{type}" if type == ("circuit" or "graph") else type
+    result_type = type if type == "latex" else f"mpl/{type}"
     result_files_circuit = []
     for file in os.listdir(os.path.join(SWD, result_type)):
         if file.endswith(".png") and not file.endswith(".diff.png"):
