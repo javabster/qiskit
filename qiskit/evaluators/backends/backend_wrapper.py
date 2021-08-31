5c3850e633f895030caa9d39234fd7d91a068e6c
Takashi Imamichi <t.imamichi@gmail.com>
2021-08-31 22:25:25 +0900

    add backend wrappers

---
+++ qiskit/evaluators/backends/backend_wrapper.py
@@ -0,0 +1,200 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""
+Backend wrapper classes
+"""
+
+import logging
+from abc import ABC, abstractmethod
+from datetime import datetime
+from time import time
+from typing import List, Union
+
+from qiskit.circuit import QuantumCircuit
+from qiskit.ignis.mitigation.measurement import (
+    CompleteMeasFitter,
+    TensoredMeasFitter,
+    complete_meas_cal,
+    tensored_meas_cal,
+)
+from qiskit.providers.backend import BackendV1
+from qiskit.providers.ibmq.job import IBMQJobApiError, IBMQJobFailureError, IBMQJobInvalidStateError
+from qiskit.result import Result
+
+logger = logging.getLogger(__name__)
+
+
+class BaseBackendWrapper(ABC):
+    @abstractmethod
+    def run_and_wait(
+        self, circuits: Union[QuantumCircuit, List[QuantumCircuit]], **options
+    ) -> Result:
+        NotImplemented
+
+    @property
+    @abstractmethod
+    def backend(self) -> BackendV1:
+        NotImplemented
+
+
+class BackendWrapper(BaseBackendWrapper):
+    def __init__(self, backend: BackendV1):
+        self._backend = backend
+
+    @property
+    def backend(self) -> BackendV1:
+        return self._backend
+
+    def run_and_wait(
+        self, circuits: Union[QuantumCircuit, List[QuantumCircuit]], **options
+    ) -> Result:
+        job = self._backend.run(circuits, **options)
+        return job.result()
+
+    @classmethod
+    def from_backend(cls, backend: Union[BackendV1, BaseBackendWrapper]) -> BaseBackendWrapper:
+        if isinstance(backend, BackendV1):
+            return cls(backend)
+        return backend
+
+
+class Retry(BaseBackendWrapper):
+    def __init__(self, backend: BackendV1):
+        self._backend = backend
+
+    @property
+    def backend(self):
+        return self._backend
+
+    @staticmethod
+    def _get_result(job):
+        """Get a result of a job. Will retry when ``IBMQJobApiError`` (i.e., network error)
+
+        ``IBMQJob.result`` raises the following errors.
+            - IBMQJobInvalidStateError: If the job was cancelled.
+            - IBMQJobFailureError: If the job failed.
+            - IBMQJobApiError: If an unexpected error occurred when communicating with the server.
+        """
+        while True:
+            try:
+                return job.result()
+            except IBMQJobApiError as ex:  # network error, will retry to get a result
+                logger.warning(ex.message)
+
+    def run_and_wait(
+        self, circuits: Union[QuantumCircuit, List[QuantumCircuit]], **options
+    ) -> Result:
+        while True:
+            job = self._backend.run(circuits, **options)
+            try:
+                result = self._get_result(job)
+            except IBMQJobInvalidStateError as ex:  # cancelled, will retry to submit a job
+                logger.warning(ex.message)
+                logger.info("Job was cancelled %s. Retry another job.", job.job_id())
+                continue
+            except IBMQJobFailureError as ex:  # job failed, will terminate
+                logger.warning(ex.message)
+                raise ex
+
+            if result.success:
+                return result
+            else:
+                logger.warning("job finished unsuccessfully %s", job.job_id())
+
+
+class ReadoutErrorMitigation(BaseBackendWrapper):
+    # need to move to the new mitigator class in the future
+    # https://github.com/Qiskit/qiskit-terra/pull/6485
+    # need to support M3 https://github.com/Qiskit-Partners/mthree
+    def __init__(
+        self,
+        backend: Union[BackendV1, BaseBackendWrapper],
+        mitigation: str,
+        refresh: float,
+        shots: int,
+        **cal_options,
+    ):
+        self._backend = BackendWrapper.from_backend(backend)
+        self._mitigation = mitigation
+        self._refresh = refresh
+        self._shots = shots
+        self._time_threshold = 0.0
+        self._cal_options = cal_options
+        self._meas_fitter = {}
+
+    @property
+    def backend(self):
+        if isinstance(self._backend, BaseBackendWrapper):
+            return self._backend.backend
+        return self._backend
+
+    @property
+    def mitigation(self):
+        return self._mitigation
+
+    @property
+    def refresh(self):
+        return self._refresh
+
+    @property
+    def cal_options(self):
+        return self._cal_options
+
+    @property
+    def shots(self):
+        return self._shots
+
+    @staticmethod
+    def _datetime(data):
+        # Aer's result.data is str, but IBMQ's result.data is datetime
+        if isinstance(data, str):
+            return datetime.fromisoformat(data)
+        return data
+
+    def _maybe_calibrate(self):
+        now = time()
+        if now <= self._time_threshold:
+            return
+        if self._mitigation == "tensored":
+            meas_calibs, state_labels = tensored_meas_cal(**self._cal_options)
+        elif self._mitigation == "complete":
+            meas_calibs, state_labels = complete_meas_cal(**self._cal_options)
+
+        logger.info("readout error mitigation calibration %s at %f", self._mitigation, now)
+        cal_results = self._backend.run_and_wait(meas_calibs, shots=self._shots)
+
+        dt = self._datetime(cal_results.date)
+        if self._mitigation == "tensored":
+            self._meas_fitter[dt] = TensoredMeasFitter(cal_results, **self._cal_options)
+        elif self._mitigation == "complete":
+            self._meas_fitter[dt] = CompleteMeasFitter(
+                cal_results, state_labels, **self._cal_options
+            )
+        self._time_threshold = now + self._refresh
+
+    def _apply_mitigation(self, result: Result):
+        result_dt = self._datetime(result.date)
+        fitters = [
+            (abs(date - result_dt), date, fitter) for date, fitter in self._meas_fitter.items()
+        ]
+        _, min_date, min_fitter = min(fitters, key=lambda e: e[0])
+        logger.info("apply mitigation data at %s", min_date)
+        return min_fitter.filter.apply(result)
+
+    def apply_mitigation(self, results: List[Result]):
+        return [self._apply_mitigation(result) for result in results]
+
+    def run_and_wait(
+        self, circuits: Union[QuantumCircuit, List[QuantumCircuit]], **options
+    ) -> Result:
+        self._maybe_calibrate()
+        return self._backend.run_and_wait(circuits, **options)
---
+++ qiskit/evaluators/backends/shot_backend_wrapper.py
@@ -0,0 +1,144 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""
+Shot Backend wrapper class
+"""
+
+import logging
+from collections import Counter
+from typing import List, Tuple, Union
+
+from qiskit.circuit import QuantumCircuit
+from qiskit.exceptions import QiskitError
+from qiskit.providers.backend import BackendV1
+from qiskit.result import Counts, Result
+
+from .backend_wrapper import BackendWrapper, BaseBackendWrapper, ReadoutErrorMitigation
+
+logger = logging.getLogger(__name__)
+
+
+class ShotBackendWrapper:
+    """Backend wrapper to return a list of counts"""
+
+    def __init__(self, backend: Union[BackendV1, BaseBackendWrapper]):
+        self._backend = BackendWrapper.from_backend(backend)
+
+        config = self._backend.backend.configuration()
+        self._max_shots = config.max_shots
+        if hasattr(config, "max_experiments"):
+            self._max_experiments = config.max_experiments
+        else:
+            logger.warning("no max_experiments for this backend")
+            self._max_experiments = 1
+        self._num_circuits = 0
+        self._num_splits = 0
+        self._raw_results = []
+
+    @property
+    def backend(self):
+        return self._backend
+
+    @property
+    def max_shots(self):
+        return self._max_shots
+
+    @property
+    def max_experiments(self):
+        return self._max_experiments
+
+    @property
+    def raw_results(self):
+        return self._raw_results
+
+    def _split_experiments(
+        self, circuits: List[QuantumCircuit], shots: int
+    ) -> List[Tuple[List[QuantumCircuit], int]]:
+        assert self._num_circuits > self._max_experiments
+        ret = []
+        remaining_shots = shots
+        splits = []
+        for i in range(0, self._num_circuits, self._max_experiments):
+            splits.append(circuits[i : min(i + self._max_experiments, self._num_circuits)])
+        self._num_splits = len(splits)
+        logger.info("Number of splits: %d", self._num_splits)
+        while remaining_shots > 0:
+            shots = min(remaining_shots, self._max_shots)
+            remaining_shots -= shots
+            for circs in splits:
+                ret.append((circs, shots))
+        return ret
+
+    def _copy_experiments(
+        self, circuits: List[QuantumCircuit], shots: int
+    ) -> List[Tuple[List[QuantumCircuit], int]]:
+        assert self._num_circuits <= self._max_experiments
+        max_copies = self._max_experiments // self._num_circuits
+        ret = []
+        remaining_shots = shots
+        while remaining_shots > 0:
+            num_copies, rem = divmod(remaining_shots, self._max_shots)
+            if rem:
+                num_copies += 1
+            num_copies = min(num_copies, max_copies)
+
+            shots, rem = divmod(remaining_shots, num_copies)
+            if rem:
+                shots += 1
+            shots = min(shots, self._max_shots)
+            logger.info(
+                "Number of shots: %d, number of copies: %d, total number of shots: %d",
+                shots,
+                num_copies,
+                shots * num_copies,
+            )
+            remaining_shots -= shots * num_copies
+            ret.append((circuits * num_copies, shots))
+        return ret
+
+    def run_and_wait(
+        self, circuits: Union[QuantumCircuit, List[QuantumCircuit]], append: bool = False, **options
+    ) -> List[Counts]:
+        if "shots" in options:
+            shots = options["shots"]
+            del options["shots"]
+        else:
+            shots = self._backend.backend.options.shots
+        if isinstance(circuits, QuantumCircuit):
+            circuits = [circuits]
+        self._num_circuits = len(circuits)
+        if self._num_circuits > self._max_experiments:
+            circs_shots = self._split_experiments(circuits, shots)
+        else:
+            circs_shots = self._copy_experiments(circuits, shots)
+        results = []
+        for circs, shots in circs_shots:
+            result = self._backend.run_and_wait(circs, shots=shots, **options)
+            results.append(result)
+        if isinstance(self._backend, ReadoutErrorMitigation):
+            results = self._backend.apply_mitigation(results)
+        if append:
+            self._raw_results.extend(results)
+        else:
+            self._raw_results = results
+        return self.get_counts(self._raw_results)
+
+    def get_counts(self, results: List[Result]) -> List[Counts]:
+        if len(results) == 0:
+            raise QiskitError("Empty result")
+        counters = [Counter() for _ in range(self._num_circuits)]
+        i = 0
+        for result in results:
+            for count in result.get_counts():
+                counters[i % self._num_circuits].update(count)
+                i += 1
+        return [Counts(c) for c in counters]
